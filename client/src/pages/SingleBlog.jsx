6f1d3d163deeec9b4d9a63c02087b90eb0d11d45
Daniel Park <danielapark87@gmail.com>
2024-02-01 19:23:07 -0600

    Initial commit. Front end skeleton using ReactJS

---
+++ client/src/pages/SingleBlog.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+
+const SingleBlog = () => {
+
+  
+        return (
+            <div></div>
+        )
+};
+
+export default SingleBlog;
+
+
